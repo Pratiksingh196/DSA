7d4597fbd7a10604a16489755ba7b37e1e9fbaca
Pratiksingh196 <160873720+Pratiksingh196@users.noreply.github.com>
2026-03-29 13:14:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (67.10%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        if(s1[0]==s2[2] && s1[1]==s2[3] && s2[0]==s1[2] && s2[1]==s1[3]) {
-            return true;
-        }else if(s1[0]==s2[0] && s2[2]==s1[2] && s1[1]==s2[3] && s2[1]==s1[3] ){
-            return true;
-        }else if(s1[1]==s2[1] && s2[3]==s1[3] && s1[0]==s2[2]  && s2[0]==s1[2]){
-            return true;
-        }else if(s1[0]==s2[0] && s1[1]==s2[1] && s1[2]==s2[2] && s1[3]==s2[3]){
+        // if(s1[0]==s2[2] && s1[1]==s2[3] && s2[0]==s1[2] && s2[1]==s1[3]) {
+        //     return true;
+        // }else if(s1[0]==s2[0] && s2[2]==s1[2] && s1[1]==s2[3] && s2[1]==s1[3] ){
+        //     return true;
+        // }else if(s1[1]==s2[1] && s2[3]==s1[3] && s1[0]==s2[2]  && s2[0]==s1[2]){
+        //     return true;
+        // }else if(s1[0]==s2[0] && s1[1]==s2[1] && s1[2]==s2[2] && s1[3]==s2[3]){
+        //     return true;
+        // }else{
+        //     return false;
+        // }
+            
+        for(int i = 0 ; i <2 ; i++){
+            if(s1[i]!=s2[i]){
+                swap(s1[i],s1[i+2]);
+            }
+        }
+        if(s1==s2){
             return true;
         }else{
             return false;
         }
-            
-        
     }
 };
