ba3647281e701530e365e1fb7bb7f36ae9a4fe18
Pratik Singh <160873720+Pratiksingh196@users.noreply.github.com>
2026-07-26 09:45:44 +0530

    Time: 0 ms (100.00%) | Memory: 31.6 MB (46.18%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -1,9 +1,44 @@
+// class Solution {
+// public:
+//     int maximumProduct(vector<int>& nums) {
+//         sort(nums.rbegin() , nums.rend());
+//         int n = nums.size();
+//         return max(nums[n-1]*nums[n-2]*nums[0],nums[0]*nums[1]*nums[2]);
+        
+//     }
+// };
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        sort(nums.rbegin() , nums.rend());
+        int firstMax = INT_MIN;
+        int secondMax = INT_MIN;
+        int thirdMax = INT_MIN;
+
+        int firstMin = INT_MAX;
+        int secondMin = INT_MAX;
+
         int n = nums.size();
-        return max(nums[n-1]*nums[n-2]*nums[0],nums[0]*nums[1]*nums[2]);
-        
+        for(int i = 0 ; i<n ; i++){
+            if(nums[i] >= firstMax){
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = nums[i];
+            }
+            else if(nums[i] >= secondMax){
+                thirdMax = secondMax;
+                secondMax = nums[i];
+            }
+            else if(nums[i] > thirdMax)thirdMax = nums[i];
+            if(nums[i] <= firstMin){
+                secondMin = firstMin;
+                firstMin = nums[i];
+            }
+            else if(nums[i] < secondMin){
+                secondMin = nums[i];
+            }
+        }
+        int a = firstMax*secondMax*thirdMax;
+        int b = firstMax*firstMin*secondMin;
+        return max(a , b);
     }
 };
